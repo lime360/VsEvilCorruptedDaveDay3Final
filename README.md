7cace9ee6751fcba370377812672498b591a58da
Kiril Misnikov <156070238+lime360@users.noreply.github.com>
2024-04-02 13:35:09 +0300

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gapple 1.0 fork lol
+i made this fork with a single purpose: making the source code work well with the compiler and removing unneccesary files and libraries that only used in the vanilla game.
---
+++ art/InstallLibraries.bat
@@ -0,0 +1,10 @@
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib install flixel-tools
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install actuate
+haxelib git extension-webm https://github.com/ThatRozebudDude/extension-webm
+haxelib git discord_rpc https://github.com/Afford-Set/linc_discord-rpc
+pause
